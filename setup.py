635a06792d0f5b89b6a83a691cea96945b48c251
BOTTOMFRAGGER523 <dastancoding@gmail.com>
2024-11-26 22:19:30 +0700

    I am dumb

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='1.0',
     py_modules=['hackery'],
     description='A simple random number generator that prints 0 or 1 in green text, note it will not be recieving any updates',
-    author='BOTTOMFRAGGER523'
+    author='BOTTOMFRAGGER523',
     url='https://github.com/BOTTOMFRAGGER523/This-will-make-you-look-like-a-hacker'
 )

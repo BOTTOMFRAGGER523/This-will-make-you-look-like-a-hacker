d1a4693064032a8165e7fb8c31d3f1b79311fbc8
BOTTOMFRAGGER523 <dastancoding@gmail.com>
2024-11-26 22:05:51 +0700

    Added the GitHub repo

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(
     py_modules=['hackery'],
     description='A simple random number generator that prints 0 or 1 in green text, note it will not be recieving any updates',
     author='BOTTOMFRAGGER523'
+    url='https://github.com/BOTTOMFRAGGER523/This-will-make-you-look-like-a-hacker'
 )
